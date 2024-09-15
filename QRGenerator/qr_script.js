2b53c5ef0540f86ba4b428795471647da13d793e
nhaluan <152849574+nhaluan@users.noreply.github.com>
2024-09-15 16:12:01 +0700

    Change a little

---
+++ QRGenerator/qr_script.js
@@ -0,0 +1,46 @@
+let imgBox = document.getElementById("imgBox");
+let qrImg = document.getElementById("qrImg");
+let qrText = document.getElementById("qrText");
+let aTag = document.getElementById("hrefImg");
+
+document.addEventListener("DOMContentLoaded", function(){
+    qrText.addEventListener("input", function(){
+        aTag.href = this.value;
+    });
+});
+
+function showErrorMessage(message) {
+    const errorBox = document.getElementById("error-message-box");
+    const errorMessage = document.getElementById("error-message-text");
+    errorMessage.textContent = message;
+    errorBox.style.display = "block";
+    setTimeout(function () {
+        hideErrorMessage()
+    }, 4000);
+}
+
+function hideErrorMessage() {
+    document.getElementById("error-message-box").style.display = "none";
+}
+
+function validateURLWithRegex(url) {
+    const regex = /^(https?|ftp):\/\/[^\s/$.?#].\S*$/i;
+    return regex.test(url);
+}
+
+function generateQR() {
+    if (validateURLWithRegex(qrText.value)) {
+        imgBox.classList.add("show-img");
+        qrImg.alt = qrText.value;
+        qrImg.title = qrText.value;
+        let width = imgBox.offsetWidth;
+        let height = imgBox.offsetHeight;
+        qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=" + width + "x" + height +"&data=" + qrText.value;
+    } else {
+        if (imgBox.classList.contains("show-img")) {
+            imgBox.classList.remove("show-img");
+        }
+        showErrorMessage("Invalid URL. Please try again another URL.");
+    }
+}
+
